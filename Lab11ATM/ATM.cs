a033c948832f4c7d1dd70f5b87635e1ef439a736
Michael <Michael@acdmail.com>
2020-02-04 11:10:40 -0500

    Adding git ignore

--- Lab11ATM/ATM.cs
+++ Lab11ATM/ATM.cs
@@ -6,10 +6,25 @@ namespace Lab11ATM
     public class ATM
     {
         //fields
-        //private List<Account> userAccounts;
+        private Account loggedInAcc;
+        public bool loggedInState;
+        private List<Account> userAccounts = new List<Account>();
+        private List<string> loggedInMenu = new List<string>
+        {
+            "1: Check Balance",
+            "2: Deposit",
+            "3: Withdraw",
+            "2: Log Out",
+            "3: Exit"
+        };
+        private List<string> loggedOutMenu = new List<string>
+        {
+            "1: Add New",
+            "2: Log Out",
+            "3: Exit"
+        };
 
         //properties
-        private List<Account> UserAccounts { get; set; }
 
         //constructors
         public ATM()
@@ -18,7 +33,79 @@ namespace Lab11ATM
         }
 
         //methods
+        public void PrintMenu()
+        {
+            if (loggedInAcc == null)
+            {
+                foreach (string s in loggedOutMenu)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            else
+            {
+                foreach (string s in loggedInMenu)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
 
-        
+        public void MakeNewAccount(string name, string pw)
+        {
+            Account newAcc = new Account(name, pw);
+            userAccounts.Add(newAcc);
+            //return to menu
+        }
+
+        public void LogIn(string name, string pw)
+        {
+            for (int i = 0; i < userAccounts.Count; i++)
+            {
+                if (userAccounts[i].Name == name && userAccounts[i].Password == pw)
+                {
+                    loggedInAcc = userAccounts[i];
+                    loggedInState = true;
+                }
+            }
+        }
+
+        public void LogOut()
+        {
+            loggedInAcc = null;
+            loggedInState = false;
+        }
+
+        public void CheckBalance()
+        {
+            if (loggedInState == true)
+            {
+                Console.WriteLine(loggedInAcc.Balance);
+            }
+        }
+
+        public void Deposit(int input)
+        {
+            if (loggedInState == true)
+            {
+                loggedInAcc.Balance += input;
+            }
+        }
+
+        public void Withdraw(int input)
+        {
+            if (loggedInState == true)
+            {
+                try
+                {
+                    loggedInAcc.Balance -= input;
+                }
+                catch
+                {
+                    Console.WriteLine("Insufficient funds... big sad...");
+                }
+            }
+        }
+
     }
 }
--- Lab11ATM/Program.cs
+++ Lab11ATM/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab11ATM
 {
@@ -6,7 +7,68 @@ namespace Lab11ATM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ATM myATM = new ATM();
+
+            myATM.MakeNewAccount(GetUserInput("New Account Name: "), GetUserInput("New Password: "));
+
+            if (myATM.loggedInState == false)
+            {
+                if (YesOrNo("Do you want to log out? yes/no", "yes", "no") == true)
+                {
+                    myATM.LogOut();
+                    //return to menu
+                }
+            }
+            else
+            {
+                myATM.LogIn(GetUserInput("Account Name: "), GetUserInput("Password: "));
+
+                if (myATM.loggedInState == false)
+                {
+                    Console.WriteLine("Incorrect username and/or password");
+                    //return to menu
+                }
+            }
+
+            myATM.CheckBalance();
+            myATM.Deposit(GetUserInt("How much?: "));
+        }
+
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static int GetUserInt(string message)
+        {
+            int num = 0;
+            try
+            {
+                num = int.Parse(GetUserInput(message));
+            }
+            catch
+            {
+                GetUserInt(message);
+            }
+            return num;
+        }
+
+        public static bool YesOrNo(string message, string yes, string no)
+        {
+            string userContinue = null;
+            while (userContinue != yes && userContinue != yes)
+            {
+                Console.Write(message);
+                userContinue = Console.ReadLine().ToLower();
+
+                if (userContinue == no)
+                {
+                    Console.WriteLine("Okay!!");
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
